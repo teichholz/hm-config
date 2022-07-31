a9f58e02ca062fe1d12c4fdec1b934f18987a628
Tim Eichholz <tim.eichholz98@web.de>
2022-07-31 13:29:50 +0200

    icomoon font

--- desktop/awesome.nix
+++ desktop/awesome.nix
@@ -2,11 +2,41 @@
 
 {
 	imports = [ ./rofi.nix ];
-
 	
 	config = {
 
-		home.packages = with pkgs; [rofi dunst];
+		home.packages = with pkgs; [ 
+			acpi acpica-tools # system tools i.e battery
+			lxappearance # MAYBE DELETE appearance tool for gtk3
+			gpick # colorpicker
+			ffmpeg # video converter
+			inotify-tools # notification
+			upower # battery monitor tool
+			polkit_gnome # policiy toolkit
+			redshift # reduces blue in monitorscreen in the evening
+			blueman # bluetooth manager
+			pipewire
+			pamixer # audio mixer
+			brightnessctl
+			feh
+			maim # screenshot tool
+			mpv mpd mpc-cli mpdris2 ncmpcpp playerctl # music
+			python39Packages.mutagen
+			
+			## fonts
+			roboto
+			material-design-icons
+		];
+
+		# install icomoon font
+		home.file.".local/share/fonts/icomoon" = {
+			source = ./fonts/icomoon;
+			recursive = true;
+		};
+
+		services.mpd = {
+			enable = true;
+		};
 
 		services.picom = {
 			enable = true;
